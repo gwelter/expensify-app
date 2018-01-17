bdbb1b0bb8d9fcebe5887895df51d25972830043
Guilherme André Welter <guilherme.a.welter@gmail.com>
2018-01-17 16:39:47 -0200

    Starting with reducers

---
+++ src/playground/desctructuring.js
@@ -0,0 +1,12 @@
+const person = {
+    name: 'Guilherme',
+    age: 26,
+    location: {
+        city: 'Floripa',
+        temp: 25
+    }
+};
+
+const {name, age} = person;
+
+console.log(`${name} is ${age}`);
