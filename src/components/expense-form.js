6e1e4fbcd21e87af562d8af6ea0ba0cc65003404
Guilherme André Welter <guilherme.a.welter@gmail.com>
2018-01-19 07:18:06 -0200

    Criando form para salvar despesas

--- src/components/expense-form.js
+++ src/components/expense-form.js
@@ -1,18 +1,60 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
 	state = {
 		description: '',
-
+		note: '',
+		amount: '',
+		createdAt: moment(),
+		focused: false,
+		error: ''
 	};
 	onDescriptionChange = (e) => {
 		const description = e.target.value;
 		this.setState(() => ({ description }));
 	};
+	onNoteChange = (e) => {
+		const note = e.target.value;
+		this.setState(() => ({ note }));
+	};
+	onAmountChange = (e) => {
+		const amount = e.target.value;
+		if(!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+			this.setState(() => ({amount}));
+		}
+	};
+	onDateChangeHandler = (createdAt) => {
+		if(createdAt)
+			this.setState(() => ({createdAt}));
+	};
+	onSubmitHandler = (e) => {
+		e.preventDefault();
+		
+		if(!this.state.description || !this.state.amount) {
+			this.setState(() => ({error: 'Please provide description and amount'}));
+		} else {
+			this.setState(() => ({error: ''}));
+
+			this.props.onSubmit({
+				description: this.state.description,
+				amount: parseFloat(this.state.amount),
+				createdAt: this.state.createdAt.valueOf(),
+				note: this.state.note
+			});
+		}
+	};
 	render() {
 		return (
 			<div>
-				<form>
+				{this.state.error && <h3>ERROR MESSAGE</h3>}
+				<form onSubmit={this.onSubmitHandler}>
 					<input
 						type="text"
 						placeholder="Description"
@@ -20,9 +62,22 @@ export default class ExpenseForm extends React.Component {
 						onChange={this.onDescriptionChange}
 						autoFocus />
 					<input
-						type="number"
-						placeholder="Amount" />
-					<textarea placeholder="Add a note for your expense (optional)">
+						type="text"
+						placeholder="Amount"
+						value={this.state.amount}
+						onChange={this.onAmountChange} />
+					<SingleDatePicker
+						date={this.state.createdAt} // momentPropTypes.momentObj or null
+						onDateChange={this.onDateChangeHandler} // PropTypes.func.isRequired
+						focused={this.state.focused} // PropTypes.bool
+						onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+						numberOfMonths={1}
+						isOutsideRange={() => false}
+					/>
+					<textarea
+						placeholder="Add a note for your expense (optional)"
+						value={this.state.note}
+						onChange={this.onNoteChange}>
 					</textarea>
 					<button>Add Expense</button>
 				</form>
